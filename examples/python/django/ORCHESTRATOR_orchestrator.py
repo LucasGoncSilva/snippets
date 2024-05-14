a2da73582bcbd8e186b2fa43e89c0849d2011f10
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2024-05-13 22:32:29 -0300

    Deployed 9bc0db9 with MkDocs version: 1.6.0

--- examples/python/django/ORCHESTRATOR_orchestrator.py
+++ examples/python/django/ORCHESTRATOR_orchestrator.py
@@ -5,28 +5,40 @@ from sys import argv
 
 
 environ['DEBUG'] = 'True'
-environ['ALLOWED_HOSTS'] = '* 127.0.0.1'
-environ['DJANGO_SETTINGS_MODULE'] = '*****.settings.base'
+environ['ALLOWED_HOSTS'] = '*'
+environ['DJANGO_SETTINGS_MODULE'] = 'CORE.settings.dev'
 
-environ['EMAIL_HOST_USER'] = '*****'
-environ['EMAIL_HOST_PASSWORD'] = '*****'
+# environ['EMAIL_HOST_USER'] = ''
+# environ['EMAIL_HOST_PASSWORD'] = ''
 
 
 system('black -S .')
-# system('find -name "*.pyc" -type f -delete')
+system('find -name "*.pyc" -type f -delete')
 system('rm -rf `find -type d -name __pycache__`')
 
 
-def collectstatic():
+def run_false():
     system('python3 manage.py collectstatic --noinput')
 
+
 def makemigrations():
-    exclude_list = ['.git', '.github', '.vscode', 'templates', 'static', 'CORE', 'LIPSUM', 'requirements', 'staticfiles', 'env']
+    exclude_list = [
+        'templates',
+        'static',
+        'CORE',
+        'requirements',
+        'staticfiles',
+        'env',
+        'media',
+        'htmlcov',
+    ]
     apps = [d for d in listdir('.') if isdir(d) and d not in exclude_list]
     system('python3 manage.py makemigrations ' + ' '.join(apps))
 
+
 def migrate():
-    system('python3 manage.py migrate')    
+    system('python3 manage.py migrate')
+
 
 def test():
     makemigrations()
@@ -34,26 +46,66 @@ def test():
     environ['DEBUG'] = 'False'
     system('python3 manage.py collectstatic --noinput')
 
+
 def cleardb():
     system('rm -rf `find -type d -name migrations -not -path "./env/*"`')
     system('find -name "*.sqlite3" -type f -delete')
     exit(0)
 
+
 def populatedb():
     makemigrations()
     migrate()
-    system(...)
+    system('python3 manage.py populateuser')
+    system('python3 manage.py populatesecret')
+
 
 def docker():
-    system(...)
-
-if argv[1] == 'runserver' and environ.get('DEBUG') == 'False': collectstatic()
-elif argv[1] == 'makemigrations': makemigrations(); exit()
-elif argv[1] == 'test': test()
-elif argv[1] == 'cleardb': cleardb()
-elif argv[1] == 'populatedb': populatedb(); exit()
-elif argv[1] == 'docker': docker(); system('python3 manage.py ' + ' '.join([i for i in argv[1:]])); exit()
+    system('docker compose -f docker-compose-dev.yml up')
 
 
-sleep(10)
+def coverage():
+    module = argv[2]
+    omit_list = [
+        'orchestrator.py',
+        '*/migrations/*',
+        'manage.py',
+        '*/CORE/*',
+    ]
+    test()
+    system(
+        f'coverage run --source=\'{module}\' --omit=\'{",".join(omit_list)}\' manage.py test {module}'
+    )
+    system('coverage html')
+
+
+def gunicorn():
+    system('gunicorn CORE.wsgi 0.0.0.0:8000')
+
+
+if argv[1] == 'runserver' and environ.get('DEBUG') == 'False':
+    run_false()
+elif argv[1] == 'makemigrations':
+    makemigrations()
+    exit()
+elif argv[1] == 'test':
+    test()
+elif argv[1] == 'cleardb':
+    cleardb()
+elif argv[1] == 'populatedb':
+    populatedb()
+    exit()
+elif argv[1] == 'docker':
+    docker()
+    system('python3 manage.py ' + ' '.join([i for i in argv[1:]]))
+    exit()
+elif argv[1] == 'coverage':
+    coverage()
+    exit()
+elif argv[1] == 'gunicorn':
+    gunicorn()
+    exit()
+
+
+sleep(3)
 system('python3 manage.py ' + ' '.join([i for i in argv[1:]]))
--- examples/sh/start/expo_start.sh
+++ examples/sh/start/expo_start.sh
@@ -13,24 +13,198 @@ npx create-expo-app $APP_NAME -t expo-template-blank-typescript
 cd $APP_NAME
 
 mkdir src src/screens src/components
+mkdir src/components/buttons src/components/texts src/components/icons
 
 npm i nativewind
 npm i tailwindcss@3.3.2 --save-dev
 npm i @react-navigation/native-stack
 npx tailwind init
 
-echo "import { View, Text, Button } from 'react-native';
+
+echo "import { Text } from \"react-native\";
+
+function Paragraph(props: any) {
+  return (
+    <Text className=\"text-secondary text-justify text-lg mb-4\">
+      {props.children}
+    </Text>
+  );
+}
+
+export { Paragraph };" > src/components/texts/Paragraph.tsx
+
+
+echo "import { Text } from \"react-native\";
+
+interface SubTitleProps {
+  text: string;
+  className?: string;
+}
+
+function SubTitle(props: SubTitleProps) {
+  return (
+    <Text className={\`text-2xl mb-5 text-primary \${props.className}\`}>
+      {props.text}
+    </Text>
+  );
+}
+
+export { SubTitle };" > src/components/texts/SubTitle.tsx
+
+
+echo "import { Text } from \"react-native\";
+
+interface TitleProps {
+  text: string;
+  className?: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+    <Text className={\`text-4xl mt-10 mb-5 text-primary \${props.className}\`}>
+      {props.text}
+    </Text>
+  );
+}
+
+export { Title };" > src/components/texts/Title.tsx
+
+
+echo "import { Feather } from \"@expo/vector-icons\";
+import colors from \"tailwindcss/colors\";
+
+interface IconProps {
+  name: keyof typeof Feather.glyphMap;
+  size?: number;
+}
+
+function Icon(props: IconProps) {
+  return (
+    <Feather
+      name={props.name}
+      size={props.size ? props.size : 50}
+      color={colors.blue[500]}
+    />
+  );
+}
+
+export { Icon };" > src/components/icons/Icon.tsx
+
+
+echo "import { TouchableOpacity, Text, GestureResponderEvent } from \"react-native\";
+
+interface TertiaryButtonProps {
+  text: string;
+  btnClassName?: string;
+  textClassName?: string;
+  onPress?: (event: GestureResponderEvent) => void;
+}
+
+function TertiaryButton(props: TertiaryButtonProps) {
+  return (
+    <TouchableOpacity
+      className={\`border-2 my-2 mx-5 border-transparent rounded-xl \${props.btnClassName}\`}
+      onPress={props.onPress}
+    >
+      <Text
+        className={\`text-xl text-center text-secondary py-2 px-5 \${props.textClassName}\`}
+      >
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+export { TertiaryButton };" > src/components/buttons/TertiaryButton.tsx
+
+
+echo "import { TouchableOpacity, Text, GestureResponderEvent } from \"react-native\";
+
+interface SecondaryButtonProps {
+  text: string;
+  btnClassName?: string;
+  textClassName?: string;
+  onPress?: (event: GestureResponderEvent) => void;
+}
+
+function SecondaryButton(props: SecondaryButtonProps) {
+  return (
+    <TouchableOpacity
+      className={\`border-2 border-solid border-secondary my-2 mx-5 rounded-xl \${props.btnClassName}\`}
+      onPress={props.onPress}
+    >
+      <Text
+        className={\`text-xl text-center text-secondary py-2 px-5 \${props.textClassName}\`}
+      >
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+export { SecondaryButton };" > src/components/buttons/SecondaryButton.tsx
+
+
+echo "import { TouchableOpacity, Text, GestureResponderEvent } from \"react-native\";
+
+interface PrimaryButtonProps {
+  text: string;
+  btnClassName?: string;
+  textClassName?: string;
+  onPress?: (event: GestureResponderEvent) => void;
+}
+
+function PrimaryButton(props: PrimaryButtonProps) {
+  return (
+    <TouchableOpacity
+      className={\`border-2 border-solid border-primary bg-primary rounded-xl my-2 mx-5 \${props.btnClassName}\`}
+      onPress={props.onPress}
+    >
+      <Text
+        className={\`text-xl text-center text-baseColor py-2 px-5 \${props.textClassName}\`}
+      >
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+export { PrimaryButton };" > src/components/buttons/PrimaryButton.tsx
+
+
+echo "import { SafeAreaView, ScrollView } from \"react-native\";
+
+import { Title } from \"../components/texts/Title\";
+import { SubTitle } from \"../components/texts/SubTitle\";
+import { PrimaryButton } from \"../components/buttons/PrimaryButton\";
+import { SecondaryButton } from \"../components/buttons/SecondaryButton\";
+import { TertiaryButton } from \"../components/buttons/TertiaryButton\";
+import { Icon } from \"../components/icons/Icon\";
+import { Paragraph } from \"../components/texts/Paragraph\";
 
 export function Home({ navigation }: { navigation: any }) {
   return (
-    <View className=\"flex-1 justify-center items-center\" >
-      <Text>Home</Text>
-      <Button
-        title=\"Go to Details\"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  )
+    <SafeAreaView className=\"bg-baseColor px-10 py-5 h-full w-full\">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Title text=\"Title\" />
+        <SubTitle text=\"Subtitle\" />
+        <PrimaryButton
+          text=\"Primary Btn\"
+          onPress={() => navigation.navigate(\"Details\")}
+        />
+        <SecondaryButton text=\"Secondary Btn\" />
+        <TertiaryButton text=\"Tertiary Btn\"/>
+        <Icon name=\"home\" />
+        <Paragraph>
+          Donec mollis ante dictum dapibus gravida. Pellentesque commodo dolor
+          vel ante molestie, ac lacinia diam ullamcorper. Phasellus ante urna,
+          lobortis sit amet cursus in, consequat non libero. Nam accumsan mauris
+          sed consectetur sodales. Cras erat quam, rutrum ultrices tempor a,
+          efficitur eget metus.
+        </Paragraph>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }" > src/screens/Home.tsx
 
 echo "import { View, Text, Button } from 'react-native';
@@ -47,19 +221,22 @@ export function Details({ navigation }: { navigation: any }) {
   )
 }" > src/screens/Details.tsx
 
-echo "import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+echo "import * as React from \"react\";
+import { NavigationContainer } from \"@react-navigation/native\";
+import { createNativeStackNavigator } from \"@react-navigation/native-stack\";
+import { StatusBar } from \"expo-status-bar\";
+import colors from \"tailwindcss/colors\";
 
-import { Home } from './src/screens/Home';
-import { Details } from './src/screens/Details';
+import { Home } from \"./src/screens/Home\";
+import { Details } from \"./src/screens/Details\";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerTransparent: true, title: '' }}>
+      <StatusBar style=\"dark\" backgroundColor={colors.blue[200]} />
+      <Stack.Navigator screenOptions={{ headerTransparent: true, title: \"\" }}>
         <Stack.Screen name=\"Home\" component={Home} />
         <Stack.Screen name=\"Details\" component={Details} />
         {/* Other screens here */}
@@ -71,16 +248,23 @@ export default function App() {
 echo "/// <reference types=\"nativewind/types\" />" > app.d.ts
 
 echo "/** @type {import('tailwindcss').Config} */
+
+const colors = require(\"tailwindcss/colors\");
+
 module.exports = {
-  content: [
-    \"./App.tsx\",
-    \"./src/**/*.tsx\"
-  ],
+  content: [\"./App.tsx\", \"./src/**/*.tsx\"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        baseColor: colors.blue[200],
+        primary: colors.blue[700],
+        secondary: colors.blue[500],
+      },
+    },
   },
   plugins: [],
-}" > tailwind.config.js
+};
+" > tailwind.config.js
 
 echo "module.exports = function(api) {
   api.cache(true);
